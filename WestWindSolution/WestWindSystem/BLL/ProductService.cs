9412b75040e84ba891fde36ce0c8940167f00ac5
Sam Wu <swu@nait.ca>
2024-11-25 14:48:51 -0700

    Updated ProductService

--- WestWindSolution/WestWindSystem/BLL/ProductService.cs
+++ WestWindSolution/WestWindSystem/BLL/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,9 +22,50 @@ namespace WestWindSystem.BLL
         {
             var query = _context
                 .Products
-                .Where(x => x.CategoryID == categoryID)
+                //.Where(p => p.Discontinued == false)
+                //.Where(x => x.CategoryID == categoryID)
+                .Where(p => p.Discontinued == false && p.CategoryID == categoryID)
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .AsNoTracking()
                 .OrderBy(x => x.ProductName);
             return query.ToList();
         }
+
+        #region CRUD methods
+        private void ValidateProduct(Product existingProduct)
+        {
+            if (existingProduct == null)
+            {
+                throw new ArgumentNullException(nameof(existingProduct),"A product is required");
+            }
+        }
+        public int AddProduct(Product newProduct)
+        {
+            ValidateProduct(newProduct);
+            newProduct.Discontinued = false;
+            _context.Products.Add(newProduct);
+            _context.SaveChanges();
+            return newProduct.ProductID;
+        }
+        public int UpdateProduct(Product existingProduct)
+        {
+            ValidateProduct(existingProduct);
+            _context.Products.Update(existingProduct);
+            return _context.SaveChanges();
+        }
+        public int DiscontinueProduct(int productID)
+        {
+            Product? existingProduct = _context.Products.FirstOrDefault(p => p.ProductID == productID);
+            if (existingProduct != null)
+            {
+                existingProduct.Discontinued = true;
+                _context.Products.Update(existingProduct);
+            }
+            return _context.SaveChanges();
+        }
+
+
+        #endregion
     }
 }
---
+++ WestWindSolution/WestWindSystem/BLL/SupplierService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    public class SupplierService
+    {
+        private readonly WestWindContext _context;
+
+        internal SupplierService(WestWindContext registerContext)
+        {
+            _context = registerContext;
+        }
+
+        public List<Supplier> GetAll()
+        {
+            var query = _context
+                .Suppliers
+                .OrderBy(s => s.CompanyName);
+            return query.ToList();
+        }
+    }
+}
